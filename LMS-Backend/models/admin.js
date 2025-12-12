2541c74dc40666e18ef49eb5094e733fa1aec76b
Prince Benjamin K <princebenjamin.k2024cse@sece.ac.in>
2025-12-12 10:42:42 +0530

    Added backend structure and initial User/Admin schemas

---
+++ LMS-Backend/models/admin.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Admin = new mongoose.Schema({
+  firstName: {
+    type: String,
+    require: true,
+  },
+  secondName: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  mobileNumber: {
+    type: String,
+    require: true,
+  },
+  collegeName: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+module.exports = mongoose.model("Admin", Admin);
---
+++ LMS-Backend/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const User = new mongoose.Schema({
+  firstName: {
+    type: String,
+    require: true,
+  },
+  secondName: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  mobileNumber: {
+    type: String,
+    require: true,
+  },
+  collegeName: {
+    type: String,
+    require: true,
+  },
+  active: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+module.exports = mongoose.model("User", User);
